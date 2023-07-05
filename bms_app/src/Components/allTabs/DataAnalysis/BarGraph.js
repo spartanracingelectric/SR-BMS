720ae1fd7771134b8f8682629274d529671a538e
Sensei-Josh <89555034+Sensei-Josh@users.noreply.github.com>
2023-07-05 10:23:16 -0700

    Updated link

--- bms_app/src/Components/allTabs/DataAnalysis/BarGraph.js
+++ bms_app/src/Components/allTabs/DataAnalysis/BarGraph.js
@@ -20,7 +20,7 @@ ChartJS.register(
 function BarGraph() {
 {/* 
 	
-	link: 
+	link: https://www.youtube.com/watch?v=svJRmxrPUFw
 	2 ways to display data:
 		1. have 3 seperate tabs for each graph
 		2. display all 3 in one graph
