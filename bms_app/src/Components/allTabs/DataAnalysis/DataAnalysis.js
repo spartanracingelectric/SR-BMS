72161851da1c2dcc7f284165c425dbae3d2d5c8a
Sensei-Josh <joshjeffrey29@gmail.com>
2023-07-29 15:23:23 -0700

    created states for graph, graph more dynamic

--- bms_app/src/Components/allTabs/DataAnalysis/DataAnalysis.js
+++ bms_app/src/Components/allTabs/DataAnalysis/DataAnalysis.js
@@ -6,6 +6,58 @@ import DataTitles from './StaticData.js';
 let initialData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function DataAnalysis() {
+	/*
+		Vdata, Idata, IVdata hold the graph data for the BMS
+		Refresh() updates these states
+		Note: not sure if we need IVdata if we have Voltage and Current data
+	*/
+	const [Vdata, setVdata] = useState (initialData);
+	const [Idata, setIdata] = useState (initialData);
+	const [IVdata, setIVdata] = useState (initialData);
+	
+	// barData and options hold the displayed graph data
+	const [barData, setBarData] = useState ({
+		labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+		datasets: [ 
+			{
+				label: 'Voltage',
+				data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				backgroundColor: 'white'
+			}
+			 
+		]
+	});
+	
+	const [options, setOptions] = useState ({
+		scales: {
+			y: {
+				ticks: {
+					color: 'black'
+				},
+				grid: {
+					color: 'grey'
+				},
+				title: {
+					display: true,
+					text: 'N/A',
+					color: 'black'
+				}
+			},
+			x: {
+				ticks: {
+					color: 'black'
+				},
+				grid: {
+					color: 'grey'
+				},
+				title: {
+					display: true,
+					text: 'Cell Size',
+					color: 'black'
+				}
+			}
+		}
+	});
 	
 	/*
 		Data array = [Vpack, Cpack, Ppack, CVhigh,
@@ -24,6 +76,121 @@ function DataAnalysis() {
 		(0.00).toFixed(2), (1.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (3.00).toFixed(2)]
 		
 		SetData(newData);
+	}
+
+	const changeState1 = () => {
+		setBarData(barData => {
+			return{
+				labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+				datasets: [{
+					label: 'Voltage',
+					data: Vdata,
+					backgroundColor: 'white'
+				}]
+			}
+		});
+		setOptions(options => {
+			return{ 
+				...options,
+				scales: {
+					...options.scales,
+						y: {
+							...options.scales.y,
+								title: {
+									...options.scales.y.title,
+										text: 'Voltage (V)'
+								}
+						}
+				}
+			}
+		});
+	}
+
+	const changeState2 = () => {
+		setBarData(barData => {
+			return{
+				labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+				datasets: [{
+					label: 'Temperature',
+					data: Idata,
+					backgroundColor: 'darkgreen'
+				}]
+			}
+		});
+		setOptions(options => {
+			return{ 
+				...options,
+				scales: {
+					...options.scales,
+						y: {
+							...options.scales.y,
+								title: {
+									...options.scales.y.title,
+										text: 'Temperature'
+								}
+						}
+				}
+			}
+		});
+	}
+
+	const changeState3 = () => {
+		setBarData(barData => {
+			return{
+				labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+				datasets: [{
+					label: 'IV',
+					data: IVdata,
+					backgroundColor: 'lightgreen'
+				}]
+			}
+		});
+		setOptions(options => {
+			return{ 
+				...options,
+				scales: {
+					...options.scales,
+						y: {
+							...options.scales.y,
+								title: {
+									...options.scales.y.title,
+										text: 'Current (i)'
+								}
+						},
+						x: {
+							...options.scales.x,
+							title: {
+								...options.scales.x.title,
+								text: 'Voltage (V)'
+							}
+						}
+				}
+			}
+		});	
+	}
+	
+	
+	function UpdateVoltage() {
+		const n1Data = [Math.floor(Math.random() * 5), 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3];
+		setVdata(n1Data);
+	}
+	
+	function UpdateCurrent() {
+		const n2Data = [Math.floor(Math.random() * 5), 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1];
+		setIdata(n2Data);
+	}
+	
+	function UpdateIV() {
+		const n3Data = [Math.floor(Math.random() * 5), 1, 2, -3, 1, -2, 3, 1, 2, -3];
+		setIVdata(n3Data);
+	}
+
+	function Refresh() {
+		UpdateVoltage();
+		UpdateCurrent();
+		UpdateIV();
+		
+		UpdateData();
 	}
 
 	return (
@@ -62,7 +229,8 @@ function DataAnalysis() {
 			&nbsp;
 			&nbsp;
 			&nbsp;
-			<BarGraph UpdateData = {UpdateData}/>
+			<BarGraph Refresh = {Refresh} changeState1 = {changeState1} changeState2 = {changeState2} 
+				changeState3 = {changeState3} barData = {barData} options = {options}/>
 		</div>
 	);
 }
