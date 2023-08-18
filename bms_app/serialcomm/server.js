5ee254b14e86ceccbd9e53b1ca47c77552ff77c0
Sensei-Josh <joshjeffrey29@gmail.com>
2023-08-17 19:23:16 -0700

    serialport comm w/ arduino 1

--- bms_app/serialcomm/server.js
+++ bms_app/serialcomm/server.js
@@ -1,24 +1,53 @@
 const express = require('express');
 const cors = require('cors');
 
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
+
+const EventEmitter = require('events');
+
+const insert = new EventEmitter();
+
 const app = express();
 const port = 8000;
-
-function getData() {
-	var data = {
-		volt: [Math.floor(Math.random() * 5), 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
-		temp: [Math.floor(Math.random() * 5), 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1],
-		curr: [Math.floor(Math.random() * 5), 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2],
-		static: [(3.00).toFixed(2), (0.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2),
-			(2.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (1.00).toFixed(2), (3.00).toFixed(2),
-			(0.00).toFixed(2), (1.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (3.00).toFixed(2)]
-	};
-	
-	return data;	
-}
-
 app.use(cors());
 app.use(express.json());
+
+const myPort = new SerialPort({path: "COM5", 
+	baudRate: 9600
+});
+const parser = myPort.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+var data = {
+	volt: [Math.floor(Math.random() * 5), 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
+	temp: [Math.floor(Math.random() * 5), 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1],
+	curr: [Math.floor(Math.random() * 5), 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2],
+	static: [(3.00).toFixed(2), (0.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2),
+		(2.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (1.00).toFixed(2), (3.00).toFixed(2),
+		(0.00).toFixed(2), (1.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (3.00).toFixed(2)]
+};
+
+var strindex = 0;
+var valdex = 0;
+var text = "";
+
+insert.on('start', () => {
+	var x = parseInt(text, 2);
+	console.log(x);
+	//console.log(valdex);
+	strindex = 0;
+	data.static[valdex] = x.toFixed(2);
+	valdex++;
+	text = "";
+	if(valdex == 14)
+		valdex = 0;
+});
+
+function getData() {
+	myPort.write("h");
+
+	return data;	
+}
 
 app.get('/graphdata', (req, res) => {
     res.json(getData());
@@ -27,3 +56,19 @@ app.get('/graphdata', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
+
+myPort.on('open', onOpen);
+parser.on('data', onData);
+
+function onOpen() {
+	console.log("Open Connection");
+}
+
+function onData(data) {
+	console.log("Got: " + data);
+	text = text + data;
+	strindex++;
+	if(strindex == 8) {
+		insert.emit('start');
+	}
+}
