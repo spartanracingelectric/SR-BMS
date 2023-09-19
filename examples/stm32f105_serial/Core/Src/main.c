8988115efed99116d0a0411a72bb47d8c6375b37
vsawce <vsaw30@gmail.com>
2023-09-19 00:00:32 -0700

    Added functional serial echo example using USB Virtual COM port

--- examples/stm32f105_serial/Core/Src/main.c
+++ examples/stm32f105_serial/Core/Src/main.c
@@ -23,7 +23,7 @@
 #include "spi.h"
 #include "tim.h"
 #include "usart.h"
-#include "usb_otg.h"
+#include "usb_device.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -109,8 +109,7 @@ int main(void)
   MX_SPI1_Init();
   MX_USART2_UART_Init();
   MX_USART3_UART_Init();
-  MX_USB_OTG_FS_PCD_Init();
-
+  MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
   //Start timer
