bf4013be6cf85961d99226c7604191342e38b761
Abegail Palad <78325753+abbsgp@users.noreply.github.com>
2023-12-09 00:06:44 -0800

    Update server.js
    updated onData function to parse incoming serial data, com port (subject to change based on computer's device manager), simulated data commented out

--- gui/bms_app/serialcomm/server.js
+++ gui/bms_app/serialcomm/server.js
@@ -1,37 +1,54 @@
-const express = require('express');			//REST API for communication between frontend and backend
-const cors = require('cors');				//ensures we are sending right headers
-
+const express = require('express');
+const cors = require('cors');
 const { SerialPort } = require("serialport");
 const { ReadlineParser } = require("@serialport/parser-readline");
+const EventEmitter = require('events');
 
+const insert = new EventEmitter();
 
 const app = express();
 const port = 8000;
+
+const MAX_VAL_VOLTS = 65535;
+
 app.use(cors());
 app.use(express.json());
 
-//Initialize serial port used
 const myPort = new SerialPort({
-	path: "COM5", 
-	baudRate: 9600
-});
-const parser = myPort.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+    path: "COM6", //Change
+    baudRate: 115200
 
-//object that stores the data sent from board
-var dataPackage = {
-	volt: [Math.floor(Math.random() * 5), 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3],
-	temp: [Math.floor(Math.random() * 5), 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1],
-	curr: [Math.floor(Math.random() * 5), 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2],
-	static: [(3.00).toFixed(2), (0.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2),
-		(2.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (1.00).toFixed(2), (3.00).toFixed(2),
-		(0.00).toFixed(2), (1.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (3.00).toFixed(2)]
+});
+const parser = myPort.pipe(new ReadlineParser({ delimiter: '\n' })); //Unix style new-line
+
+var data = {
+    volt: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], //Default 0v/no connection
+    temp: [Math.floor(Math.random() * 5), 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1],
+    curr: [Math.floor(Math.random() * 5), 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2],
+    static: [(3.00).toFixed(2), (0.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2),
+        (2.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (1.00).toFixed(2), (3.00).toFixed(2),
+        (0.00).toFixed(2), (1.00).toFixed(2), (1.00).toFixed(2), (2.00).toFixed(2), (3.00).toFixed(2)]
 };
 
-//Send signal (h) to board to begin sending data
+var strindex = 0;
+var valdex = 0;
+var text = "";
+
+insert.on('start', () => {
+    var x = parseInt(text, 2);
+    console.log(x);
+    strindex = 0;
+    data.static[valdex] = x.toFixed(2);
+    valdex++;
+    text = "";
+    if (valdex == 14)
+        valdex = 0;
+});
+
 function getData() {
-	myPort.write("h");
-	
-	return dataPackage;	
+    myPort.write("h");
+    return data;
+
 }
 
 //frontend requests data to /graphdata to which backend responds and sends data object
@@ -42,7 +59,7 @@ app.get('/graphdata', (req, res) => {
 
 //backend goes to listening mode and "listens" for data sent from board
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+    console.log(`Server is running on port ${port}.`);
 });
 
 myPort.on('open', onOpen);
@@ -52,24 +69,52 @@ var arrayIndex = 0;
 var mode = "";
 
 function onOpen() {
-	console.log("Open Connection");
+    console.log("Open Connection");
+
+    //Simulate receiving the specified data directly in the code
+    //const simulatedData = "$c1=34615,c2=34615,c3=34615,c4=34615,c5=34615,c6=34615,c7=34615,c8=34615,c9=34615,c10=34615,c11=34615,c12=34615,@\n";
+    //onData();
 }
 
-//sent data is dealt with this function
-function onData(data) {
-	//console.log("Got: " + data);
+function onData(receivedData) {
+    console.log("Got: " + receivedData);
 
-	if (data === "v" || data === "t" || data === "i") {
-		mode = data;
-	}
+    if (receivedData === undefined) {
+        return;
+    }
 
-	if(mode === "v") {
-		dataPackage.volt[arrayIndex] = data;
-	} else if (mode === "t") {
-		dataPackage.temp[arrayIndex] = data;
-	} else if (mode === "i") {
-		dataPackage.curr[arrayIndex] = data;
-	}
+    text = text + receivedData;
+    strindex++;
 
-	arrayIndex = (arrayIndex + 1) % 12;
+    // Check if the received data contains the start and end markers
+    if (text.startsWith("$")) {
+
+        // Extract the data between '$' and '@'
+        const extractedData = text.substring(text.indexOf("$")+1, text.indexOf("@"));
+
+        // Split the data into individual components
+        const components = extractedData.split(',');
+
+        // Process each component and update the data object
+        components.forEach(component => {
+            const [key, value] = component.split('=');
+            // Update data based on the key
+            if (key.startsWith('c')) {
+                const index = parseInt(key.slice(1)) - 1; // Convert 'c1', 'c2', etc. to array index
+                // Update voltage data
+                data.volt[index] = (parseInt(value) === MAX_VAL_VOLTS) ? 0 : parseInt(value) * 0.0001; 
+            }
+        });
+
+        //console.log(data);
+
+        // Reset the text and strindex for the next data packet
+        text = "";
+        strindex++;
+
+        // Emit 'start' event 
+        if (strindex == 12) {
+            insert.emit('start');
+        }
+    }
 }
